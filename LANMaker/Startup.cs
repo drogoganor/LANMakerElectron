1729211403b515a7be6a25a1698114a95bf9c358
David Wilson <spamdog@gmail.com>
2022-08-07 17:17:11 +1000

    Initial commit

---
+++ LANMaker/Startup.cs
@@ -0,0 +1,21 @@
+﻿using ElectronNET.API;
+
+namespace LANMaker
+{
+    public static class Startup
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            //rest of your ConfigureServices method
+            services.AddElectron();
+        }
+
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //...
+
+            // Open the Electron-Window here
+            Task.Run(async () => await Electron.WindowManager.CreateWindowAsync());
+        }
+    }
+}
